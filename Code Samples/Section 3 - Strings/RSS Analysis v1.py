e1e908b415b6d840d7eb6db6f750c175de3680cd
Oide Brett <oideibrett@lccgmail.com>
2018-05-29 11:47:41 +0100

    new string files

---
+++ Code Samples/Section 3 - Strings/RSS Analysis v1.py
@@ -0,0 +1,58 @@
+from urllib.request import urlopen
+from xml.dom import minidom
+import collections
+
+# extract the headlines from the feed
+def extractString(doc):
+   str = ""
+   for node in doc.getElementsByTagName('channel'):
+      for title in node.getElementsByTagName('title'):
+         str = str + title.firstChild.data + "\n"
+   return str
+
+# extract the feed from the url
+def getRSSString(url):
+    results = []
+    rssString = ""
+    results.append(minidom.parse(urlopen(url)))
+    for webDoc in results:
+        rssString = rssString + extractString(webDoc)
+    return rssString
+
+
+
+# START HERE ...
+feedFile = open ("feeds.txt","r")
+feedURL = feedFile.readline()
+feed = getRSSString(feedURL)
+
+
+
+# Read the RSS feed from the URL provided
+#feed = getRSSString("https://www.apple.com/main/rss/hotnews/hotnews.rss")
+
+# Display the results
+print(feed.capitalize())
+print(feed.upper())
+print(feed.lower())
+
+feed = feed.lower()
+vowelCount = 0
+vowelCount = vowelCount + feed.count('a')
+vowelCount = vowelCount + feed.count('e')
+vowelCount = vowelCount + feed.count('i')
+vowelCount = vowelCount + feed.count('o')
+vowelCount = vowelCount + feed.count('u')
+
+print("vowel count= ", vowelCount)
+
+
+#print(feed.replace("Garda","cop"))
+
+# Display the number of lines
+#print("There are %d lines in this feed" %feed.count("\n"))
+
+words = feed.split()
+print(collections.Counter(words).most_common(5))
+
+feedFile.close()
---
+++ Code Samples/Section 4 - Lists/fruitExperimentp107.py
@@ -0,0 +1,16 @@
+fruits = ['Strawberry','Lemon','Orange','Raspberry','Cherry']
+
+print(fruits[0])
+print(fruits[3])
+print(fruits[2])
+print(fruits[len(fruits)-1])
+print(fruits[1])
+
+fruit = fruits[2+2]
+print(fruit)
+print(fruit[0])
+
+orange = fruits[1]
+print(orange)
+lemon = fruits[1]
+print(lemon)
