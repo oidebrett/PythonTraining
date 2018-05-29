f104a07692ed6b61bbf040069ac415028dc730ae
Oide Brett <oideibrett@lccgmail.com>
2018-05-29 09:30:43 +0100

    StringConcat

---
+++ Code Samples/Section 3 - Strings/stringConstruction.py
@@ -0,0 +1,14 @@
+#INitialize the output string
+
+outStr = "The quick "
+
+colour = input("Please eneter a colour:")
+
+outStr = outStr + colour
+outStr = outStr + " fox jumps over the lazy "
+
+animal = input("Please enter an animal:")
+outStr = outStr + animal
+outStr = outStr + "!"
+
+print(outStr)
