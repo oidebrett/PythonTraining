01d07d112a59ece3fd388392e6b84aeaaa5a4639
Oide Brett <oideibrett@lccgmail.com>
2018-05-29 11:58:26 +0100

    cards example

---
+++ Code Samples/Section 4 - Lists/cardsp108.py
@@ -0,0 +1,15 @@
+suits = ['Hearts','Diamonds','Spades','Clubs']
+cardFaces = ['Ace','1','2','3','4','5','6','7','8','9','Jack','Queen','King']
+
+print(cardFaces[1:10])
+
+colourCards = cardFaces[10:23]
+print(colourCards)
+
+redSuits = suits[:2]
+#blackSuits = [2:]
+blackSuits = suits[2:]
+
+#print(pinStripSuits)
+print(redSuits)
+print(blackSuits)
