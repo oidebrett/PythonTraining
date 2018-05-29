2db4fa7c245c463d2287d346299d70665dffca9f
Oide Brett <oideibrett@lccgmail.com>
2018-05-29 09:44:30 +0100

    initial files

---
+++ Code Samples/Section 3 - Strings/caesarCipher.py
@@ -0,0 +1,13 @@
+inStr = input("Enter any character: ")
+outStr = chr(ord(inStr)+1)
+print(outStr)
+
+inStr = input("Enter a 6 letter string: ")
+outStr = ""
+outStr = outStr + chr(ord(inStr[0])+1)
+outStr = outStr + chr(ord(inStr[1])+1)
+outStr = outStr + chr(ord(inStr[2])+1)
+outStr = outStr + chr(ord(inStr[3])+1)
+outStr = outStr + chr(ord(inStr[4])+1)
+outStr = outStr + chr(ord(inStr[5])+1)
+print(outStr)
